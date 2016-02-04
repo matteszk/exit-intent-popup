8886ed26f839ae51b72cb8a06eec43acfb2f2c3c
brunogianni <bruno.gianni@resultadosdigitais.com.br>
2016-02-04 13:41:47 -0200

    modify loading events of popup modify triggering of popup to show on exit intent despite the timing

--- js/bioep.js
+++ js/bioep.js
@@ -6,7 +6,7 @@ window.bioEp = {
 	shown: false,
 	overflowDefault: "visible",
 	transformDefault: "",
-	
+
 	// Popup options
 	width: 400,
 	height: 220,
@@ -16,43 +16,43 @@ window.bioEp = {
 	delay: 5,
 	showOnDelay: false,
 	cookieExp: 30,
-	
+
 	// Object for handling cookies, taken from QuirksMode
 	// http://www.quirksmode.org/js/cookies.html
 	cookieManager: {
 		// Create a cookie
 		create: function(name, value, days) {
 			var expires = "";
-			
+
 			if(days) {
 				var date = new Date();
 				date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
 				expires = "; expires=" + date.toGMTString();
 			}
-			
+
 			document.cookie = name + "=" + value + expires + "; path=/";
 		},
-		
+
 		// Get the value of a cookie
 		get: function(name) {
 			var nameEQ = name + "=";
 			var ca = document.cookie.split(";");
-			
+
 			for(var i = 0; i < ca.length; i++) {
 				var c = ca[i];
 				while (c.charAt(0) == " ") c = c.substring(1, c.length);
 				if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
 			}
-			
+
 			return null;
 		},
-		
+
 		// Delete a cookie
 		erase: function(name) {
 			this.create(name, "", -1);
 		}
 	},
-	
+
 	// Handle the bioep_shown cookie
 	// If present and true, return true
 	// If not present or false, create and return false
@@ -62,17 +62,17 @@ window.bioEp = {
 			this.cookieManager.erase("bioep_shown");
 			return false;
 		}
-		
+
 		// If cookie is set to true
 		if(this.cookieManager.get("bioep_shown") == "true")
 			return true;
-			
+
 		// Otherwise, create the cookie and return false
 		this.cookieManager.create("bioep_shown", "true", this.cookieExp);
-		
+
 		return false;
 	},
-	
+
 	// Add font stylesheets and CSS for the popup
 	addCSS: function() {
 		// Add font stylesheets
@@ -83,7 +83,7 @@ window.bioEp = {
 			font.rel = "stylesheet";
 			document.head.appendChild(font);
 		}
-		
+
 		// Base CSS styles for the popup
 		var css = document.createTextNode(
 			"#bio_ep_bg {display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: #000; opacity: 0.3; z-index: 10001;}" +
@@ -91,24 +91,24 @@ window.bioEp = {
 			"#bio_ep_close {position: absolute; left: 100%; margin: -8px 0 0 -12px; width: 20px; height: 20px; color: #fff; font-size: 12px; font-weight: bold; text-align: center; border-radius: 50%; background-color: #5c5c5c; cursor: pointer;}" +
 			this.css
 		);
-		
+
 		// Create the style element
 		var style = document.createElement("style");
 		style.type = "text/css";
 		style.appendChild(css);
-		
+
 		// Insert it before other existing style
 		// elements so user CSS isn't overwritten
 		document.head.insertBefore(style, document.getElementsByTagName("style")[0]);
 	},
-	
+
 	// Add the popup to the page
 	addPopup: function() {
 		// Add the background div
 		this.bgEl = document.createElement("div");
 		this.bgEl.id = "bio_ep_bg";
 		document.body.appendChild(this.bgEl);
-		
+
 		// Add the popup
 		if(document.getElementById("bio_ep"))
 			this.popupEl = document.getElementById("bio_ep");
@@ -118,40 +118,40 @@ window.bioEp = {
 			this.popupEl.innerHTML = this.html;
 			document.body.appendChild(this.popupEl);
 		}
-		
+
 		// Add the close button
 		this.closeBtnEl = document.createElement("div");
 		this.closeBtnEl.id = "bio_ep_close";
 		this.closeBtnEl.appendChild(document.createTextNode("X"));
 		this.popupEl.insertBefore(this.closeBtnEl, this.popupEl.firstChild);
 	},
-	
+
 	// Show the popup
 	showPopup: function() {
 		if(this.shown) return;
-		
+
 		this.bgEl.style.display = "block";
 		this.popupEl.style.display = "block";
-		
+
 		// Handle scaling
 		this.scalePopup();
-		
+
 		// Save body overflow value and hide scrollbars
 		this.overflowDefault = document.body.style.overflow;
 		document.body.style.overflow = "hidden";
-		
+
 		this.shown = true;
 	},
-	
+
 	// Hide the popup
 	hidePopup: function() {
 		this.bgEl.style.display = "none";
 		this.popupEl.style.display = "none";
-		
+
 		// Set body overflow back to default to show scrollbars
 		document.body.style.overflow = this.overflowDefault;
 	},
-	
+
 	// Handle scaling the popup
 	scalePopup: function() {
 		var margins = { width: 40, height: 40 };
@@ -159,19 +159,19 @@ window.bioEp = {
 		var windowSize = { width: window.innerWidth, height: window.innerHeight };
 		var newSize = { width: 0, height: 0 };
 		var aspectRatio = popupSize.width / popupSize.height;
-		
+
 		// First go by width, if the popup is larger than the window, scale it
 		if(popupSize.width > (windowSize.width - margins.width)) {
 			newSize.width = windowSize.width - margins.width;
 			newSize.height = newSize.width / aspectRatio;
-			
+
 			// If the height is still too big, scale again
 			if(newSize.height > (windowSize.height - margins.height)) {
 				newSize.height = windowSize.height - margins.height;
 				newSize.width = newSize.height * aspectRatio;
 			}
 		}
-		
+
 		// If width is fine, check for height
 		if(newSize.height === 0) {
 			if(popupSize.height > (windowSize.height - margins.height)) {
@@ -179,17 +179,17 @@ window.bioEp = {
 				newSize.width = newSize.height * aspectRatio;
 			}
 		}
-		
+
 		// Set the scale amount
 		var scaleTo = newSize.width / popupSize.width;
-		
+
 		// If the scale ratio is 0 or is going to enlarge (over 1) set it to 1
 		if(scaleTo <= 0 || scaleTo > 1) scaleTo = 1;
-		
+
 		// Save current transform style
-		if(this.transformDefault === "") 
+		if(this.transformDefault === "")
 			this.transformDefault = window.getComputedStyle(this.popupEl, null).getPropertyValue("transform");
-			
+
 		// Apply the scale transformation
 		this.popupEl.style.transform = this.transformDefault + " scale(" + scaleTo + ")";
 	},
@@ -201,7 +201,7 @@ window.bioEp = {
 		else if(obj.attachEvent)
 			obj.attachEvent("on" + event, callback);
 	},
-	
+
 	// Load event listeners for the popup
 	loadEvents: function() {
 		// Track mouseout event on document
@@ -218,13 +218,13 @@ window.bioEp = {
 		this.addEvent(this.closeBtnEl, "click", function() {
 			bioEp.hidePopup();
 		});
-		
+
 		// Handle window resizing
 		this.addEvent(window, "resize", function() {
 			bioEp.scalePopup();
 		});
 	},
-	
+
 	// Set user defined options for the popup
 	setOptions: function(opts) {
 		this.width = (typeof opts.width === 'undefined') ? this.width : opts.width;
@@ -241,28 +241,30 @@ window.bioEp = {
 	domReady: function(callback) {
 		(document.readyState === "interactive" || document.readyState === "complete") ? callback() : this.addEvent(document, "DOMContentLoaded", callback);
 	},
-	
+
 	// Initialize
 	init: function(opts) {
 		// Handle options
 		if(typeof opts !== 'undefined')
 			this.setOptions(opts);
-		
+
 		// Add CSS here to make sure user HTML is hidden regardless of cookie
 		this.addCSS();
-		
+
 		// Once the DOM has fully loaded
-		this.domReady(function() {	
+		this.domReady(function() {
 			// Handle the cookie
 			if(bioEp.checkCookie()) return;
-			
+
 			// Add the popup
 			bioEp.addPopup();
-			
-			// Load events
-			setTimeout(function() { 
-				bioEp.loadEvents();
 
+			// Load events
+			//load events of exit intent despite timeout
+			bioEp.loadEvents();
+
+			//set timeout to show popup
+			setTimeout(function() {
 				if(bioEp.showOnDelay)
 					bioEp.showPopup();
 			}, bioEp.delay * 1000);
